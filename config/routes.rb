82ce021f41de61bccb4ee07a83b69f5d23ee0de7
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-01-15 10:08:27 +0000

    [add]3-12レイアウト整理destroy実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   post '/books' => 'books#create'
   get '/books' => 'books#index'
   get '/books/:id' => 'books#show', as: 'book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
   end
