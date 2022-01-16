60cbb60c7c339852fd37730e953f1d31f7c09508
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-01-16 12:25:08 +0000

    [finish]3-12課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
-  get '/', to: 'homes#top', as:"homes"
+  root to: 'homes#top'
   get '/books', to: 'books#index', as:"books"
   post '/books' => 'books#create'
   get '/books' => 'books#index'
